d37c6002bd92491fb005d5f4373a271c1d3ca60f
Euphoride <hazim.g.sager@gmail.com>
2019-03-04 14:25:44 +0000

    upload files

--- _node.js
+++ _node.js
@@ -11,6 +11,65 @@ const {
     exec
 } = require("child_process");
 
+function FindClosestCity(clientLat, clientLong) {
+  fs.readFile("datastore/longlatplanes.json", function(err, data) {
+      // grab client's location through what they've posted via GET
+
+
+      console.log("Client's Location: Lat:" + clientLat + ", Long: " + clientLong + "\n");
+
+      // grab an array of data from our file
+      var parsedJSON = JSON.parse(data)['cityLocations'];
+
+      var completos  = 1000000000000000;
+      // the amount of girls
+      // i defintely pull on
+      // a daily basis
+      //
+      // A poem, by Hazim.
+
+      var cityNameo = ""
+      var numero    = 0
+
+      // iterate through all cities and locations
+      for (var i = 0; i < Object.keys(parsedJSON).length; i++) {
+
+          // this is just a simple pythagorean algorthim to figure out where the
+          // closest city is
+          var latitudes      = parsedJSON[String(i + 1)];
+
+          var latitudesArray = latitudes.split(",");
+
+          citylat  = parseFloat(latitudesArray[0]);
+          citylong = parseFloat(latitudesArray[1]);
+          cityName = latitudesArray[2];
+
+
+          var latDistance  = citylat - clientLat;
+          var longDistance = citylong - clientLong;
+
+          var completeDistance = Math.sqrt(Math.pow(latDistance, 2) + Math.pow(longDistance, 2));
+          console.log("Client's abstract distance from " + cityName + " is: " + String(completeDistance) + "\n");
+
+          if (completeDistance < completos) {
+              completos = completeDistance;
+              cityNameo = cityName;
+              console.log(cityNameo + " is closer than any other checked..." + "\n");
+
+              numero    = i;
+          } else {
+              continue
+          }
+
+      }
+
+
+      return(cityNameo);
+
+  });
+
+}
+
 app.get("*", function(req, res) {
 
     // grab the current time
@@ -26,120 +85,67 @@ app.get("*", function(req, res) {
     });
 
     if (req.query.type == "JSONDataGET") {
-        // open up our major city locations database
-        fs.readFile("datastore/longlatplanes.json", function(err, data) {
-            // grab client's location through what they've posted via GET
-            var clientLong = parseFloat(req.query.long);
-            var clientLat  = parseFloat(req.query.lat);
 
-            console.log("Client's Location: Lat:" + req.query.lat + ", Long: " + req.query.long + "\n");
+      var clientLong = parseFloat(req.query.long);
+      var clientLat  = parseFloat(req.query.lat);
+      // open up our major city locations database
 
-            // grab an array of data from our file
-            var parsedJSON = JSON.parse(data)['cityLocations'];
+      cityNameo = FindClosestCity(clientLong, clientLat);
 
-            var completos  = 1000000000000000;
-            // the amount of girls
-            // i defintely pull on
-            // a daily basis
-            //
-            // A poem, by Hazim.
+      executionString = "python3 jsoneditorget.py " + cityNameo;
+      exec(executionString, (err, stdout, stderr) => {});
 
-            var cityNameo = ""
-            var numero    = 0
+      fs.readFile("NPTI.txt", function (err, data) {
+        var dataToConsole = data.toString("utf8", 0, data.length);
 
-            // iterate through all cities and locations
-            for (var i = 0; i < Object.keys(parsedJSON).length; i++) {
-
-                // this is just a simple pythagorean algorthim to figure out where the
-                // closest city is
-                var latitudes      = parsedJSON[String(i + 1)];
-
-                var latitudesArray = latitudes.split(",");
-
-                citylat  = parseFloat(latitudesArray[0]);
-                citylong = parseFloat(latitudesArray[1]);
-                cityName = latitudesArray[2];
-
-
-                var latDistance  = citylat - clientLat;
-                var longDistance = citylong - clientLong;
-
-                var completeDistance = Math.sqrt(Math.pow(latDistance, 2) + Math.pow(longDistance, 2));
-                console.log("Client's abstract distance from " + cityName + " is: " + String(completeDistance) + "\n");
-
-                if (completeDistance < completos) {
-                    completos = completeDistance;
-                    cityNameo = cityName;
-                    console.log(cityNameo + " is closer than any other checked..." + "\n");
-
-                    numero    = i;
-                } else {
-                    continue
-                }
-
-            }
-
-            executionString = "python3 jsoneditorget.py " + cityNameo;
-            exec(executionString, (err, stdout, stderr) => {});
-
-            fs.readFile("NPTI.txt", function (err, data) {
-              var dataToConsole = data.toString("utf8", 0, data.length);
-
-              res.writeHead(200, {'Content-Type': 'text/html'});
-              res.write(dataToConsole);
-              res.end();
-            });
-        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(dataToConsole);
+        res.end();
+      });
     } else if (req.query.type == "JSONDataPUSH") {
         businessLocationLat  = req.query.lat;
         businessLocationLong = req.query.long;
 
-        var completos        = 1000000000000000;
+        cityNameo = FindClosestCity(businessLocationLat, businessLocationLong);
 
-        fs.readFile("datastore/longlatplanes.json", function(err, data) {
-            parsedJSON = JSON.parse(data)['cityLocations'];
+        dataToAddTo = cityNameo;
+        fs.readFile("businesses.json", function(err, data) {
+            // should execute a python script which edits the json file
+            // need to double check security of req.query.long/lat
 
-            // iterate through all cities and locations
-            for (var i = 0; i < Object.keys(parsedJSON).length; i++) {
-                // this is just a simple pythagorean algorthim to figure out where the
-                // closest city is
-                var latitudes      = parsedJSON[String(i + 1)];
-
-                var latitudesArray = latitudes.split(",");
-
-                cityLat  = parseFloat(latitudesArray[0]);
-                cityLong = parseFloat(latitudesArray[1]);
-                cityName = latitudesArray[2];
-
-
-                var latDistance  = businessLocationLat - cityLat;
-                var longDistance = businessLocationLong - cityLong;
-
-                var completeDistance = Math.sqrt(Math.pow(latDistance, 2) + Math.pow(longDistance, 2));
-                console.log("Client's abstract distance from " + cityName + " is: " + String(completeDistance) + "\n");
-
-                if (completeDistance < completos) {
-                    completos = completeDistance;
-                    cityNameo = cityName;
-                    console.log(cityNameo + " is closer than any other checked..." + "\n");
-                    numero    = i;
-                } else {
-                    continue;
-                }
-
-            }
-
-            dataToAddTo = cityNameo;
-            fs.readFile("businesses.json", function(err, data) {
-                // should execute a python script which edits the json file
-                // need to double check security of req.query.long/lat
-
-                executionString = "python3 jsoneditorpush.py " + dataToAddTo + " " + req.query.name + " " + businessLocationLat + " " + businessLocationLong;
-                exec(executionString, (err, stdout, stderr) => {});
+            executionString = "python3 jsoneditorpush.py " + dataToAddTo + " " + req.query.name + " " + businessLocationLat + " " + businessLocationLong;
+            exec(executionString, (err, stdout, stderr) => {
+              console.log(stdout);
             });
-
-            res.end("done");
         });
+
+        res.end("done");
+    } else if (req.query.type == "BusinessDataPUSH") {
+
+      lat  = req.query.lat;
+      long = req.query.long;
+
+      businessDetails = req.query.details;
+      businessRating  = req.query.rating;
+      businessEmailN  = req.query.emailN;                   // Email Name
+      businessEmailD  = req.query.emailD;                   // Email Domain
+      businessPhone   = req.query.telephone;
+      businessName    = req.query.name;
+
+      cityName = FindClosestCity(lat, long);
+
+      // i love how i find any excuse to pass node onto python
+
+      executionString = "python3 bisDetailPush.py " + cityName + " " + businessDetails + " " + businessRating + " " + businessEmailN + " " + businessEmailD + " " + businessPhone + " " + businessName;
+      exec(executionString, (err, stdout, stderr) +> {
+        console.log(stdout);
+      });
+
+      res.end(done);
+
+    } else {
+      res.writeHead(404, {'Content-Type':'text/html'});
+      res.end("-- You've reached the 404 wall. Either you're not invited or there's a bug in the server/request --");
     }
 }).listen(1111);
 
---
+++ bisDetailPush.py
@@ -0,0 +1,182 @@
+import os
+import sys
+import time
+
+# sub2pewdiepie
+
+ourCityFlag  = False
+tf           = True
+
+iteratorPad  = 1
+jsonFile     = "datastore/dataStoreBusiness.json"
+
+nameDepth    = 0
+tfCounter    = 0
+
+cityCount    = 0
+nonOurCount  = 0
+
+insertionData   = []
+
+city            = sys.argv[1]
+businessDetails = sys.argv[2]
+businessRating  = sys.argv[3]
+businessEmailN  = sys.argv[4]
+businessEmailD  = sys.argv[5]
+businessPhone   = sys.argv[6]
+businessName    = sys.argv[7]
+
+jsonFileHandlerR = open(jsonFile, 'r')
+
+jsonData     = jsonFileHandlerR.readlines()
+
+# file always properly starts at line 2, or jsonData[1]
+
+
+for lineCounter in range(len(jsonData) - 1):
+
+    lineCounter = lineCounter + iteratorPad
+
+    # so now we'll grab our line
+    currentLine = jsonData[lineCounter]
+
+    if currentLine == '{\n' or currentLine == '}\n':
+        continue
+    else:
+        if ourCityFlag == False:
+            currentLine = currentLine[1:]
+            # take the line, and find the ":"
+            # if it's the last character in the line
+            # then we can assume it's a city, and not a business
+
+
+            if currentLine[len(currentLine) - 2] == ":":
+                # it's a city
+                cityCount = cityCount + 1
+
+                currentCity = currentLine[2:len(currentLine) - 3]
+                # now we want to know if it's the city we want
+                if currentCity == city:
+                    ourCityFlag = True
+                    tf = False
+                else:
+                    nonOurCount = nonOurCount + 1
+            if tfCounter == 0:
+                if nonOurCount == cityCount:
+                    cityName = "  \"" + city + "\":\n"
+                    braces   = "\t{\n\t},\n"
+
+                    citySnip = [cityName, braces]
+
+                    backupp = jsonData[:1]
+                    backupf = jsonData[1:]
+
+                    actualData = backupp + citySnip + backupf
+
+                    jsonFileHandlerW = open(jsonFile, 'w')
+
+                    jsonFileHandlerW.writelines(actualData)
+
+                    jsonFileHandlerW.close()
+
+                    command = "python3 bisDetailPush.py " + city + " " + businessDetails + " " + businessRating + " " + businessEmailN + " " + businessEmailD + " " + businessPhone + " " + businessName;
+                    os.system(command)
+
+                    tf = False
+        else:
+            # now we're within our city, first we're look for the
+            # the end signal (a "}")
+
+            if currentLine == '  }\n' or currentLine == '\t},\n':
+                # here we want to know we've ended all city-related
+                # logic and also insert our business's details
+                ourCityFlag   = False
+
+                # two values we need to calculate:
+                # 1. insertion line
+                # 2. last entry (as we need to add a "," at the end)
+                insertionLine = refrenceMarker + nameDepth
+                editLine      = insertionLine  - 1
+
+                testing = []
+
+                if jsonData[editLine] != '\t{\n':
+                    ouredit = jsonData[editLine][:-1]
+
+                    ouredit = ouredit + ",\n"
+
+                    jsonData[editLine] = ouredit
+
+                businessDeets   = "\"" + "DETAIL," + businessDetails +"\",\n"
+                BDID            = "\t\t\"" + businessName + "\": " + businessDeets
+
+                businessRat     = "\"" + "RATING," + businessRating +"\",\n"
+                BRID            = "\t\t\"" + businessName + "\": " + businessRat
+
+                businessEmail   = "\"" + "EMAIL," + businessEmailN + "@" + businessEmailD +"\",\n"
+                BEID            = "\t\t\"" + businessName + "\": " + businessEmail
+
+                businessPhoneP  = "\"" + "PHONE," + businessPhone +"\"\n"
+                BPID            = "\t\t\"" + businessName + "\": " + businessPhoneP
+
+                insertionData   = BDID + BRID + BEID + BPID
+
+                testing.append(BDID)
+                testing.append(BRID)
+                testing.append(BEID)
+                testing.append(BPID[:-1] + ",\n")
+
+                try:
+                    if jsonData[insertionLine-4:insertionLine] == testing:
+                        # we don't want duplicates as it will screw with storage capacity
+                        cont = True
+                    else:
+                        backupp = jsonData[:insertionLine]
+                        backupf = jsonData[insertionLine:]
+
+                        newData = backupp
+
+                        jsonFileHandlerW = open("datastore/dataStoreBusiness.json", 'w')
+
+                        newData.append(insertionData)
+
+                        newData = newData + backupf
+
+                        jsonFileHandlerW.writelines(newData)
+
+                        jsonFileHandlerW.close()
+
+                        tfCounter = tfCounter + 1
+                except IndexError:
+                    backupp = jsonData[:insertionLine]
+                    backupf = jsonData[insertionLine:]
+
+                    newData = backupp
+
+                    jsonFileHandlerW = open("datastore/dataStoreBusiness.json", 'w')
+
+                    newData.append(insertionData)
+
+                    newData = newData + backupf
+
+                    jsonFileHandlerW.writelines(newData)
+
+                    jsonFileHandlerW.close()
+
+                    tfCounter = tfCounter + 1
+            elif currentLine == '  {\n' or currentLine == '\t{\n':
+                # here we'll use this "{" marker as a refrence
+                # point for where we should insert our details
+                #
+                # i.e if "{" is on line 3 and we have a nameDepth of 2
+                # then we'll calculate "line (1+3+2) = line 6"
+                # (the 1 to act as a buffer, as line 5 will be the last entry)
+                # and we'll insert there
+
+                refrenceMarker = lineCounter + 1        # the 1 here acts as the
+                                                        # buffer
+            else:
+                # so here we've landed a "business name: co-ordinate" line
+                nameDepth = nameDepth + 1
+
+jsonFileHandlerR.close()
